96a23ce36dca76005e30ddef35825ba43ca096ea
Eduardo <eduardo301997@gmail.com>
2023-05-19 21:03:42 -0500

    create index.js

---
+++ asset/js/index.js
@@ -0,0 +1,21 @@
+'use strict'
+
+
+const nombre = document.getElementById('nombre');
+const apellido = document.getElementById('apellido');
+const correo = document.getElementById('correo');
+const celular = document.getElementById('celular');
+const name = document.getElementById('name');
+const surname = document.getElementById('surname');
+const email = document.getElementById('email');
+const phone = document.getElementById('phone');
+const aceptar = document.getElementById('aceptar')
+
+const empty = () =>{
+    if(!nombre.value){
+        pname.innerHTML = 'el valor no puede estar vacio';
+    }
+}
+
+const onSubmit =
+aceptar.addEventListener('click',)
